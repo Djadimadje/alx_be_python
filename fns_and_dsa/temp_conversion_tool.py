e8532548e7f23408918a84c4205a47f304ab2940
Djadimadje <m.djadimadje@gmail.com>
2025-01-12 07:34:41 -0800

    Use exact pattern for CELSIUS_TO_FAHRENHEIT_FACTOR=9/5

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 # Global conversion factors
 # Script for temperature conversion with global variables
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
 def convert_to_celsius(fahrenheit):
     """
@@ -45,3 +45,4 @@ def main():
 if __name__ == "__main__":
     main()
 
+3
