59e9d858ead83d20291c528fcbde402956f0b8a2
Djadimadje <m.djadimadje@gmail.com>
2025-01-19 09:00:16 -0800

    Add python_introduction folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Perform addition
+addition_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {addition_result}")
+
+# Perform subtraction
+subtraction_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+
+# Perform multiplication
+multiplication_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
